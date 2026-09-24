98260837e041e44cff97c3aa6397ab142a20346f
Tyas <tyasastrihningkurniasih@gmail.com>
2026-09-24 14:59:56 +0700

    Add files via upload

---
+++ Tugas/ManajemenAset.java
@@ -0,0 +1,53 @@
+// Tyasastri Hning Kurniasih - L0325034
+// Praktikum 4 PBO
+
+package Praktikum4.Tugas;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ManajemenAset {
+    // attribute
+    private List<AsetIT> daftarAset;
+    
+    public ManajemenAset() {
+        this.daftarAset = new ArrayList<>();
+    }
+    
+    // objects
+    public void tambahAset(AsetIT asetBaru) {
+        daftarAset.add(asetBaru);
+    }
+    
+    // for each loop untuk menampilkan data
+    public void tampilkanSemuaAset() {
+        if(daftarAset.isEmpty()) {
+            System.out.println("Tidak ada aset.");
+            return;
+        }
+        for (AsetIT aset : daftarAset) {
+            aset.tampilkanInfoAset();
+        }
+    }
+    
+    // iterator untuk menghapus aset berdasarkan ID
+    public void hapusAset(String idAset) {
+        Iterator<AsetIT> iterator = daftarAset.iterator();
+        boolean ditemukan = false;
+        
+        while (iterator.hasNext()) {
+            AsetIT aset = iterator.next();
+            if (aset.getIDAset().equalsIgnoreCase(idAset)) {
+                iterator.remove();
+                ditemukan = true;
+                System.out.println("Aset dengan ID '" + idAset + "' telah dihapus.\n");
+                break;
+            }
+        }
+        
+        if (!ditemukan) {
+            System.out.println("Aset dengan ID '" + idAset + "'tidak ditemukan!\n");
+        }
+    }
+}
